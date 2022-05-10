7f18638e9dd4290cc6f2210edcb831ef2ae6cd28
dedtroit <create@dannymatthews.net>
2022-05-09 20:25:05 -0700

    changes to api1

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,7 +2,7 @@ import { Client } from '@notionhq/client';
 // Require the Client object from the Notion JS SDK
 
 
-exports.handler = async function (event, context) {
+export const post = async (request) => {
     const formBody = JSON.parse(request.body)
     var name = formBody.name
     const prayer = formBody.prayer
@@ -14,48 +14,51 @@ exports.handler = async function (event, context) {
     if (phone == null) {
         phone = " "
     }
-    const notion = new Client({ auth: import.meta.env.VITE_NOTION_API_KEY });
-    const databaseId = import.meta.env.VITE_NOTION_API_DATABASE;
-    (async () => {
-        const response = await notion.pages.create({
-            parent: {
-                database_id: databaseId,
-            },
-            properties: {
-                "Name": {
-                    "title": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": name
+    exports.handler = async function (event) {
+        const notion = new Client({ auth: import.meta.env.VITE_NOTION_API_KEY });
+        const databaseId = import.meta.env.VITE_NOTION_API_DATABASE;
+        (async () => {
+            const response = await notion.pages.create({
+                parent: {
+                    database_id: databaseId,
+                },
+                properties: {
+                    "Name": {
+                        "title": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": name
+                                }
                             }
-                        }
-                    ]
-                },
+                        ]
+                    },
 
-                "Prayer": {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": prayer
+                    "Prayer": {
+                        "rich_text": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": prayer
+                                },
                             },
-                        },
-                    ],
-                },
+                        ],
+                    },
 
-                "Phone": {
-                    "phone_number": phone
+                    "Phone": {
+                        "phone_number": phone
+                    }
                 }
-            }
 
-        });
-        console.log(response);
-    })();
+            });
+            console.log(response);
+        })();
+    }
+    return{
+        statusCode: 200
+    }
+
 }
-
-
-
 
 
 
